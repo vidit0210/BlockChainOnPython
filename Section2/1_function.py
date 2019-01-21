cc8cd452d536a5b74b89019ecb59100b64f0b740
Vidit Shah <vidit0210@gmail.com>
2019-01-22 00:24:07 +0530

    assignment-1

--- Section2/1_function.py
+++ Section2/1_function.py
@@ -2,10 +2,15 @@ blockChain = [[1]]
 
 
 def get_last_blockChain_value():
+    """Return Last Value from the BlockChain"""
     return blockChain[-1]
 
 
 def add_value(value=0):
+    """ Append a new Value as well as the lasty blockChain Value 
+    Arguments:
+        :value : Enter the value of the transaction Data
+    """
     blockChain.append([get_last_blockChain_value(), value])
     print(blockChain)
 
